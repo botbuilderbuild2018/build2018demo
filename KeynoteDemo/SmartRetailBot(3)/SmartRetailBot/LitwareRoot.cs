850529f3586753fc081f24a6cb63d6eefeb26e60
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-05-01 22:38:56 -0700

    Updates to stages

---
+++ KeynoteDemo/SmartRetailBot(3)/SmartRetailBot/LitwareRoot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Builder.Ai.LUIS;
+
+namespace SmartRetailBot
+{
+    public class LitwareRoot : IBot
+    {
+        private LuisModel retailModel = new LuisModel("586c6eba-c656-4a86-adc5-b963769bbae", "be30825b782843dcbbe520ac5338f567", new Uri("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/"), Microsoft.Cognitive.LUIS.LuisApiVersion.V2);
+        public async Task OnTurn(ITurnContext context)
+        {
+            switch (context.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    var luisRecognizer = new LuisRecognizer(retailModel);
+                    var luisResult = await luisRecognizer.Recognize(context.Activity.Text, System.Threading.CancellationToken.None);
+                    
+                    if (luisResult != null)
+                    {
+                        (string key, double score) topItem = luisResult.GetTopScoringIntent();
+                        switch (topItem.key)
+                        {
+                            case "Greeting":
+                                await context.SendActivity($"Hello, I'm the contoso cafe bot. How can I help you?");
+                                break;
+                            default:
+                                await context.SendActivity($"Sorry, I do not understand that.");
+                                break;
+                        }
+                    }
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    foreach (var newMember in context.Activity.MembersAdded)
+                    {
+                        if (newMember.Id != context.Activity.Recipient.Id)
+                        {
+                            await context.SendActivity("Hello and welcome to the Cafe bot.");
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ KeynoteDemo/SmartRetailBot(3)/SmartRetailBot/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.Ai.LUIS;
+using Microsoft.Bot.Builder.Ai.QnA;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Cognitive.LUIS;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SmartRetailBot
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("cafebot.bot")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            var p = Configuration["services:1:appId"];
+            Console.WriteLine($"option1 = {Configuration["name"]}");
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_ => Configuration);
+            services.AddBot<LitwareRoot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+                
+
+                /*options.Middleware.Add(new QnAMakerMiddleware(
+                                new QnAMakerMiddlewareOptions()
+                                {
+                                    SubscriptionKey = "d534abd71a0d438d95d5a001025ee074",
+                                    KnowledgeBaseId = "40080f40-0200-482e-8e55-fae74d973490",
+                                    EndActivityRoutingOnAnswer = true
+                                }));*/
+
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseBotFramework();
+        }
+    }
+}
