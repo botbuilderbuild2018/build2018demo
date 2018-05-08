aa9ea18d24c58f43be3d4fcb01d3db25e696055b
Vishwac Sena Kannan <vishwacsena@microsoft.com>
2018-05-08 16:01:51 -0700

    S7

---
+++ ContosoCafeBot_waterfall_promptTypes/Dialogs/BookTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+//using Microsoft.Bot.Builder.Prompts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Recognizers.Text;
+using Newtonsoft.Json.Linq;
+
+namespace ContosoCafeBot.Dialogs
+{
+    public class BookTable : DialogContainer
+    {
+        public BookTable()
+            : base("BookTable")
+        {
+            Dialogs.Add("BookTable",
+                new WaterfallStep[]
+                {
+                    async (dc, args, next) =>
+                    {
+                        dc.ActiveDialog.State = new Dictionary<string, object>();
+                        await dc.Prompt("textPrompt", "Sure. I can help with that. What City?");
+                    },
+                    async (dc, args, next) =>
+                    {
+                        dc.ActiveDialog.State["city"] = args["Value"];
+                        await dc.Prompt("textPrompt", "Ok. Did you have a date in mind?");
+                    },
+                    async (dc, args, next) =>
+                    {
+                        dc.ActiveDialog.State["date"] = args["Value"];
+                        await dc.Prompt("textPrompt", "What time?");
+                    },
+                    async (dc, args, next) =>
+                    {
+                        dc.ActiveDialog.State["time"] = args["Value"];
+                        await dc.Prompt("textPrompt", "How many guests?");
+                    },
+                    async (dc, args, next) =>
+                    {
+                        dc.ActiveDialog.State["partySize"] = args["Value"];
+                        var dialogState = dc.ActiveDialog.State;
+                        await dc.Prompt("textPrompt", $"Should I go ahead and book a table for {dialogState["partySize"]} in {dialogState["city"]} for {dialogState["date"]} at {dialogState["time"]}?");
+                    },
+                    async (dc, args, next) =>
+                    {
+                        var dialogState = dc.ActiveDialog.State;
+                        // TODO: Book table
+                        await dc.Context.SendActivity($"I've booked your table for for {dialogState["partySize"]} in {dialogState["city"]} for {dialogState["date"]} at {dialogState["time"]}.");
+                        await dc.End(dc.ActiveDialog.State);
+                    }
+                }
+            );
+            Dialogs.Add("textPrompt", new TextPrompt());
+        }
+    }
+}
