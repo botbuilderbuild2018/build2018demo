2efb6a3a47d8ac98bc8f7ca2845260764722e0dc
Vishwac Sena Kannan <vishwacsena@microsoft.com>
2018-05-08 19:06:10 -0700

    S1-7(2)

--- ContosoCafeBot_LUIS/Dialogs/BookTable.cs
+++ ContosoCafeBot_LUIS/Dialogs/BookTable.cs
@@ -1,68 +1,111 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
-//using Microsoft.Bot.Builder.Prompts;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Recognizers.Text;
 using Newtonsoft.Json.Linq;
+using Microsoft.Bot.Builder;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Recognizers.Text;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Prompts.Choices;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
 
 namespace ContosoCafeBot.Dialogs
 {
     public class BookTable : DialogContainer
     {
-        private cafeLUISModel._Entities lEntities;
         public BookTable()
             : base("BookTable")
         {
+
+            var promptOptions = new ChoicePromptOptions
+            {
+                Choices = new List<Choice>
+                {
+                    new Choice { Value = "Seattle" },
+                    new Choice { Value = "Bellevue" },
+                    new Choice { Value = "Renton" },
+                }
+            };
+
+            //Dialogs.Add("textPrompt", new TextPrompt());
+
+            Dialogs.Add("choicePrompt", new ChoicePrompt(Culture.English) { Style = Microsoft.Bot.Builder.Prompts.ListStyle.Auto });
+            Dialogs.Add("numberPrompt", new NumberPrompt<int>(Culture.English));
+            Dialogs.Add("timexPrompt", new TimexPrompt(Culture.English, TimexValidator));
+            Dialogs.Add("confirmationPrompt", new ConfirmPrompt(Culture.English));
+
             Dialogs.Add("BookTable",
                 new WaterfallStep[]
                 {
                     async (dc, args, next) =>
                     {
-                        // TODO: make this a choice prompt
-                        if(dc.ActiveDialog.State.ContainsKey("LuisEntities"))
-                        {
-                            lEntities = (cafeLUISModel._Entities)dc.ActiveDialog.State["LuisEntities"];
-                        }
+                        dc.ActiveDialog.State = new Dictionary<string, object>();
+                        // await dc.Prompt("textPrompt", "Sure. I can help with that. What City?");
+                        await dc.Prompt("choicePrompt", "Which of our locations would you like?", promptOptions);
+                    },
+                    async (dc, args, next) =>
+                    {
+                        var choiceResult = (FoundChoice)args["Value"];
+                        dc.ActiveDialog.State["bookingLocation"] = choiceResult.Value;
+                        await dc.Prompt("timexPrompt", "When would you like to arrive? (We open at 4PM.)",
+                            new PromptOptions { RetryPromptString = "Please pick a date in the future and a time in the evening." });
+                    },
+                    async (dc, args, next) =>
+                    {
+                        var timexResult = (TimexResult)args;
+                        var timexResolution = timexResult.Resolutions.First();
+                        var timexProperty = new TimexProperty(timexResolution.ToString());
+                        var bookingDateTime = $"{timexProperty.ToNaturalLanguage(DateTime.Now)}";
+                        dc.ActiveDialog.State["bookingDateTime"] = bookingDateTime;
 
-                        await dc.Prompt("textPrompt", "Sure. I can help with that. What City?");
+                        await dc.Prompt("numberPrompt", "How many in your party?");
                     },
                     async (dc, args, next) =>
                     {
-                        dc.ActiveDialog.State["city"] = args["Value"];
-                        await dc.Prompt("DateTimePrompt", "Ok. Did you have a date in mind?");
-                    },
-                    async (dc, args, next) =>
-                    {
-                        List<Microsoft.Bot.Builder.Prompts.DateTimeResult.DateTimeResolution> p1 = (List<Microsoft.Bot.Builder.Prompts.DateTimeResult.DateTimeResolution>)args["Resolution"];
-                        dc.ActiveDialog.State["date"] = p1[0].Value;
-                        await dc.Prompt("DateTimePrompt", "What time?");
-                    },
-                    async (dc, args, next) =>
-                    {
-                        dc.ActiveDialog.State["time"] = ((List<Microsoft.Bot.Builder.Prompts.DateTimeResult.DateTimeResolution>)args["Resolution"])[0].Value;
-                        await dc.Prompt("PartySizePrompt", "How many guests?");
-                    },
-                    async (dc, args, next) =>
-                    {
-                        dc.ActiveDialog.State["partySize"] = args["Value"];
+                        dc.ActiveDialog.State["bookingGuestCount"] = args["Value"];
                         var dialogState = dc.ActiveDialog.State;
-                        await dc.Prompt("confirmPrompt", $"Should I go ahead and book a table for {dialogState["partySize"]} in {dialogState["city"]} for {dialogState["date"]} at {dialogState["time"]}?");
+
+                        await dc.Prompt("confirmationPrompt", $"Thanks, Should I go ahead and book a table for {dialogState["bookingGuestCount"].ToString()} guests at our {dialogState["bookingLocation"].ToString()} location for {dialogState["bookingDateTime"].ToString()} ?");
                     },
                     async (dc, args, next) =>
                     {
                         var dialogState = dc.ActiveDialog.State;
-                        // TODO: evaluate outcome of confirm prompt, route to change dialog if user said no
-                        await dc.Context.SendActivity($"I've booked your table for for {dialogState["partySize"]} in {dialogState["city"]} for {dialogState["date"]} at {dialogState["time"]}.");
-                        await dc.End(dc.ActiveDialog.State);
+
+                        // TODO: Verify user said yes to confirmation prompt
+
+                        // TODO: book the table! 
+
+                        await dc.Context.SendActivity($"Thanks, I have {dialogState["bookingGuestCount"].ToString()} guests booked for our {dialogState["bookingLocation"].ToString()} location for {dialogState["bookingDateTime"].ToString()}.");
                     }
                 }
             );
-            Dialogs.Add("DateTimePrompt", new DateTimePrompt(Culture.English));
-            Dialogs.Add("PartySizePrompt", new NumberPrompt<int>(Culture.English));
-            Dialogs.Add("confirmPrompt", new ConfirmPrompt(Culture.English));
-            Dialogs.Add("textPrompt", new TextPrompt());
+        }
+        // The notion of a Validator is a standard pattern across all the Prompts
+        private static Task TimexValidator(ITurnContext context, TimexResult value)
+        {
+            var cadidates = value.Resolutions;
+
+            var constraints = new[] {
+                TimexCreator.ThisWeek(),                /* Take any entries for this week, no entries from past please */
+                TimexCreator.NextWeek(),                /* Take any entries for next week, no dates from the past please */
+                TimexCreator.Evening,                   /* Evenings only */
+                
+            };
+
+            var resolutions = TimexRangeResolver.Evaluate(cadidates, constraints);
+
+            if (resolutions.Count == 0)
+            {
+                value.Resolutions = new string[] { };
+                value.Status = Microsoft.Bot.Builder.Prompts.PromptStatus.OutOfRange;
+            }
+            else
+            {
+                value.Resolutions = new[] { resolutions.First().TimexValue };
+                value.Status = Microsoft.Bot.Builder.Prompts.PromptStatus.Recognized;
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
